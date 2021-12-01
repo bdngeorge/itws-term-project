c939cb17cbddab8bfb3fdcf3ce3c6ec937bd28ca
ravicv3 <91489331+ravicv3@users.noreply.github.com>
2021-11-30 19:18:23 -0800

    Add files via upload

--- items.sql
+++ items.sql
@@ -10,4 +10,4 @@ INSERT INTO `books` (`id`, `imgID`, `title`, `authors`, `isbn`, `subjectCode`, `
     (3, 'csci-5109354294.jpg', 'Practical Programming', 'Paul Gries, Jennifer Campbell, Jason Montojo', '9781680502688', 'csci', 'new', 'csci1100, lightly used', '30.00', 'admin@rpi.edu');
     (4, 'mgmt-5123364294.jpg', 'Principiles of Management', 'David S Bright,  Anastasia H Cortes,  Eva Hartmann', '9384781502789', 'mgmt1100', 'new', 'lightly used', '30.00', 'admin@rpi.edu'),
     (5, 'econ-5345574294.jpg', 'Principles of Microeconomics', 'N. Gregory Mankiw', '9384781502789', '9384781502789', 'econ', 'fair', 'used', '30.00', 'admin@rpi.edu'),
-(6, 'calc-5234973584.jpg', 'Calculus Third Edition', 'William L Briggs, William Briggs, Lyle Cochran, Bernard Gillett, Eric L Schulz, Eric Schulz', '9234690702489', 'math', 'fair', 'used', '30.00', 'admin@rpi.edu');s
+    (6, 'calc-5234973584.jpg', 'Calculus Third Edition', 'William L Briggs, William Briggs, Lyle Cochran, Bernard Gillett, Eric L Schulz, Eric Schulz', '9234690702489', 'math', 'fair', 'used', '30.00', 'admin@rpi.edu');s
