b956c724ee0c3e5a462dbc9cec444354e0ee0c2a
Brandon George <bdngeorge7@gmail.com>
2021-12-01 22:10:52 -0500

    one last tinker

--- catalog/uploadBooks.php
+++ catalog/uploadBooks.php
@@ -78,16 +78,7 @@
     </script>
   </head>
   <body>
-    <header>
-      <a href="../"> <h1 class="logo left">  Textbook Buddy </h1> </a>
-      <ul class="hmenu right">
-        <a href="../catalog/catalog.php"><li>Catalog</li></a>
-        <a href="../catalog/uploadBooks.php"><li>Sell</li></a>
-        <!-- if no account, this will direct login page, else accounts page -->
-        <a href="../account/account.php"><li>Account</li></a>
-      </ul>
-    </header>
-
+    <?php include("../includes/header.php"); ?>
     <section class="">
       <h2 class="bold"> Sell Book </h2>
 
